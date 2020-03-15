1ac7d2b373626dce7b136ef287670089a49b36a1
kcs66 <35410693+kcs66@users.noreply.github.com>
2020-03-15 14:03:42 -0400

    Add files via upload

--- scripts/cpuload.cpp
+++ scripts/cpuload.cpp
@@ -8,6 +8,8 @@
 #include "appScripts/youtube.hpp"
 #include "helpers/generic.hpp"
 #include "helpers/responseTime.hpp"
+
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
@@ -39,54 +41,104 @@ int main(int argc, char *argv[]) {
             governor, timeToReadTA, decreaseCpuInterval,
             decreaseCpuFrequency, increaseCpuF, userImpatienceLevel, device);
     } else {
-        app = argv[2];
-        governor = argv[3];
-        AdbManager::getInstance()->governor = governor;
-        if (cmd == "search") {
-            AdbManager::getInstance()->keyevent(3, false);
-            AdbManager::getInstance()->openAppWithShellMonkey(app);
-            std::cout << "going to sleep 2s after keyeven3" << std::endl;
-            Generic::getInstance()->sleep(2000);
-        }
-        if (cmd == "run") {
-            iteration = argv[4];
-            AdbManager::getInstance()->governor = governor;
-            if (governor == "userspace") {
-                timeToReadTA = argv[5];
-                decreaseCpuInterval = argv[6];
-                decreaseCpuFrequency = argv[7];
-                increaseCpuF = argv[8];
-                userImpatienceLevel = argv[9];
-                AdbManager::getInstance()->uimpatienceLevel =
-                stoi(userImpatienceLevel);
-                Generic::getInstance()->setUserComplaintThreshold(AdbManager::getInstance()->uimpatienceLevel);
-                Generic::getInstance()->createFile(
-                    governor, app, iteration, device, timeToReadTA,
-                    decreaseCpuInterval, decreaseCpuFrequency, increaseCpuF,
-                    userImpatienceLevel);
-            } else {
-                Generic::getInstance()->createFile(governor, app, iteration,
-                                                   device);
-            }
-            std::cout << "C++/ADB going to run " << app << endl;
-            if (app == "gmail") {
-                Gmail g;
-                g.gmailScript(device);
-            } else if (app == "chrome") {
-                Chrome chrome;
-                chrome.chromeScript(device);
-            } else if (app == "spotify") {
-                Spotify spotify;
-                spotify.spotifyScript(device);
-            } else if (app == "youtube") {
-                Youtube youtube;
-                youtube.youtubeScript(device);
-            } else if (app == "photos") {
-                Photos photos;
-                photos.photosScript(device);
-            }
-            Generic::getInstance()->file.close();
-        }
+	
+	if(cmd == "setup")
+	{
+	
+		AdbManager::getInstance()->setup = true;
+		governor = "userspace";
+		AdbManager::getInstance()->governor = governor;
+		string apps[] = {"gmail", "chrome", "spotify", "youtube", "photos"};
+		iteration = "1";
+		timeToReadTA = "5";
+		decreaseCpuInterval = "5";
+		decreaseCpuFrequency = "0";
+		increaseCpuF = "0";
+		userImpatienceLevel = "0";
+       		AdbManager::getInstance()->setGovernorInUserImpatienceApp(governor, 
+			timeToReadTA, decreaseCpuInterval, decreaseCpuFrequency, 
+			increaseCpuF, userImpatienceLevel, device);	
+		int i = 0;
+		for(i = 0; i < 5; i++)
+		{
+			std::cout << "App: " << apps[i] << std::endl;	
+			//Generic::getInstance()->createFile(governor, apps[i], iteration, device);
+			Generic::getInstance()->openToWritePerfFile(apps[i]);
+			if (apps[i] == "gmail") {
+				Gmail g;
+				g.gmailScript(device);
+			} else if (apps[i] == "chrome") {
+				Chrome chrome;
+				chrome.chromeScript(device);
+			} else if (apps[i] == "spotify") {
+				Spotify spotify;
+				spotify.spotifyScript(device);
+			} else if (apps[i] == "youtube") {
+				Youtube youtube;
+				youtube.youtubeScript(device);
+			} else if (apps[i] == "photos") {
+				Photos photos;
+				photos.photosScript(device);
+			}
+
+			//Generic::getInstance()->file.close();
+			Generic::getInstance()->closePerfFile();
+		}
+	}
+	else
+	{
+		
+		app = argv[2];
+		Generic::getInstance()->readPerfData(app);
+		governor = argv[3];
+		AdbManager::getInstance()->governor = governor;
+		if (cmd == "search") {
+		    AdbManager::getInstance()->keyevent(3, false);
+		    AdbManager::getInstance()->openAppWithShellMonkey(app);
+		    std::cout << "going to sleep 2s after keyeven3" << std::endl;
+		    Generic::getInstance()->sleep(2000);
+		}
+		if (cmd == "run") {
+		    iteration = argv[4];
+		    AdbManager::getInstance()->governor = governor;
+		    if (governor == "userspace") {
+			Generic::getInstance()->enableImpatience();
+		        timeToReadTA = argv[5];
+		        decreaseCpuInterval = argv[6];	        
+		        increaseCpuF = argv[7];
+			decreaseCpuFrequency = argv[8];
+		        userImpatienceLevel = argv[9];
+		        AdbManager::getInstance()->uimpatienceLevel =
+		        stoi(userImpatienceLevel);
+			Generic::getInstance()->setUserWaitTime(stoi(userImpatienceLevel));
+		        Generic::getInstance()->setUserComplaintThreshold(AdbManager::getInstance()->uimpatienceLevel);
+			cout << "test: " << timeToReadTA << endl;
+		        Generic::getInstance()->createFile(governor, app, iteration, device, timeToReadTA, decreaseCpuInterval, decreaseCpuFrequency, increaseCpuF, userImpatienceLevel);
+		    } else {
+		        Generic::getInstance()->createFile(governor, app, iteration,
+		                                           device);
+		    }
+		    std::cout << "C++/ADB going to run " << app << endl;
+		    if (app == "gmail") {
+		        Gmail g;
+		        g.gmailScript(device);
+		    } else if (app == "chrome") {
+		        Chrome chrome;
+		        chrome.chromeScript(device);
+		    } else if (app == "spotify") {
+		        Spotify spotify;
+		        spotify.spotifyScript(device);
+		    } else if (app == "youtube") {
+		        Youtube youtube;
+		        youtube.youtubeScript(device);
+		    } else if (app == "photos") {
+		        Photos photos;
+		        photos.photosScript(device);
+		    }
+		    Generic::getInstance()->file.close();
+        	}
+	}
+        
     }
     return 0;
 }
